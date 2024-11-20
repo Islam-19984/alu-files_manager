561f7e8c9757070d403489bbf3efca5bb1790650
PSHEMA <p.shema@alustudent.com>
2024-11-20 13:59:34 +0200

    server.js

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-import router from './routes/index';
-
 const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 5000;
+const routes = require('./routes/index');
 
-router(app);
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ extended: false, limit: '20mb' }));
+
+app.use('/', routes);
 
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
